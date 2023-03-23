5074e6912dbab137253c5ed9bd2ca6ae0de1adde
Richard Meitern <rix133@ut.ee>
2023-03-23 17:12:13 +0200

    add columns to summary table

--- R/twinR_summary.R
+++ R/twinR_summary.R
@@ -9,7 +9,7 @@
 #'
 #' @examples
 #' #See ?twinR
-#'
+#' @seealso code{\link[twinR]{build_fit_summary.table}}
 build_fit_summary.table <- function(fit) {
 
   ## extract raw model output:
@@ -88,3 +88,60 @@ build_fit_summary.table <- function(fit) {
 
   all_results
 }
+
+#' Create data summary tables
+#'
+#' This function is used to create the tables providing the details of the data used in the study.
+#'
+#' @param birth_level_data the dataset to summarize
+#'
+#' @return a `tibble`
+#' @export
+#'
+#' @examples
+#' build_data_summary.table(data_births_all)
+#' @seealso \code{\link[twinR]{build_data_summary.table }}
+build_data_summary.table <- function(birth_level_data) {
+  se <- function(x){
+    sd(x, na.rm = T)/sqrt(length(x))
+  }
+  ## add total births and twinning status to the data:
+  birth_level_data |>
+    dplyr::group_by(.data$pop, .data$maternal_id) |>
+    dplyr::mutate(total_births = dplyr::n(),
+                  twinners     = any(.data$twin),
+                  afb = min(.data$maternal_age)/12,
+                  nontwinners  = all(!.data$twin)) |>
+    dplyr::ungroup() -> data
+  
+  ## notes for ASCII encoding of unicodes:
+  # - a umlaut  = \u00e4
+  # - o umlaut  = \u00f6
+  # - o with stroke = \u00f8
+  # - oe        = \u0153
+  # - per mille = \u2030
+  
+  ## build the part of the table that contains information for all populations combined:
+  tibble::tibble("Populations" = paste0(unique(data$pop), collapse = ", "),
+                 "Dataset" = "",
+                 "Maternal birth period" = paste(min(floor(data$maternal_birthyear), na.rm = TRUE), "-", max(floor(data$maternal_birthyear), na.rm = TRUE), sep = ""),
+                 "1/4 and 3/4 quantiles for maternal birth period" = paste(quantile(data$maternal_birthyear, 1/4, na.rm = TRUE),"-", quantile(data$maternal_birthyear, 3/4, na.rm = TRUE), sep = ""),
+                 "Age at first birth (mean-SE) " =  paste(round(base::mean(data$afb, na.rm = TRUE), digits = 2L), "-", signif(se(data$afb),2), sep = ""),
+                 "Mothers" = length(unique(data$maternal_id)),
+                 "Non-twinners" = length(unique(data$maternal_id[data$nontwinners])),
+                 "Twinners" = length(unique(data$maternal_id[data$twinners])),
+                 "Twinner rate (\u2030)" = round(.data$Twinners / (.data$Twinners + .data$`Non-twinners`) * 1000, digits = 2L),
+                 "Offspring birth period" = paste(min(data$birth_year, na.rm = TRUE), "-", max(data$birth_year, na.rm = TRUE), sep = ""),
+                 "1/4 and 3/4 quantiles for offspring birth period" = paste(quantile(data$birth_year, 1/4, na.rm = TRUE),"-", quantile(data$birth_year, 3/4, na.rm = TRUE), sep = ""),
+                 "Births" = nrow(data),
+                 "Singleton births" = sum(!data$twin, na.rm = TRUE),
+                 "Twin births" = sum(data$twin, na.rm = TRUE),
+                 "Twinning rate (\u2030)" = round(mean(data$twin, na.rm = TRUE) * 1000, digits = 2L),
+                 "Total births (min-median-max)" =  paste(1, "-", round(stats::median(data$total_births, na.rm = TRUE), digits = 2L), "-", max(data$total_births, na.rm = TRUE), sep = ""),
+                 "Total births (mean-SE)" =  paste(round(base::mean(data$total_births, na.rm = TRUE), digits = 2L), "-",  signif(se(data$total_births), 2), sep = "")
+                 ) -> tbl_total
+  
+
+  tbl_total
+}
+
